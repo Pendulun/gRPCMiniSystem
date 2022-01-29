54b47ecac942f3d22f39ede98497760d763e0e74
Daniel Campos <danielcampos@dcc.ufmg.br>
2022-01-29 19:34:54 -0300

    Arrumando regras do makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,24 @@
-compileProto:
-	python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. ./protos/keyValueStore.proto
+PROTONAME:= keyValueStore
+PB2:= $(PROTONAME)_pb2.py
+PB2GRPC:= $(PROTONAME)_pb2_grpc.py
 
-run_cli_pares: 
+compileProto:
+	@python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. ./protos/$(PROTONAME).proto
+
+run_cli_pares: compileProto
 	python client.py $(arg)
 
-run_serv_pares_1: 
+run_serv_pares_1: compileProto
 	python server.py $(arg)
 
+run_serv_pares_2: compileProto
+	python server.py $(arg) 1
+
+run_serv_central: compileProto
+	python centralServer.py $(arg)
+
+run_cli_central: compileProto
+	python client.py $(arg) 1
+
 clean:
+	rm $(PB2) $(PB2GRPC)
--- server.py
+++ server.py
@@ -8,10 +8,11 @@ import keyValueStore_pb2
 import keyValueStore_pb2_grpc
 
 class KeyValueStoreServicer(keyValueStore_pb2_grpc.KeyValueStoreServicer):
-    def __init__(self, stopEvent, myAddr):
+    def __init__(self, stopEvent, myAddr, isSecondPart):
         self.pairs = {}
         self._stop_event = stopEvent
         self._myAddr = myAddr
+        self._isSecondPart = isSecondPart
     
     def Insert(self, keyValuePair, context):
         myResponse = 0
@@ -29,35 +30,41 @@ class KeyValueStoreServicer(keyValueStore_pb2_grpc.KeyValueStoreServicer):
         return keyValueStore_pb2.Value(value=result)
     
     def Activate(self, serviceActivation, context):
-        with grpc.insecure_channel(serviceActivation.serverAddr) as newChannel:
-            stub = keyValueStore_pb2_grpc.CentralServerStub(newChannel)
-            pairServer = keyValueStore_pb2.PairServer()
-            pairServer.serverAddr = self._myAddr
-            keysMessageList = []
-            for key in list(self.pairs.keys()):
-                pairServer.keys.add().key=key
-            response = stub.Register(pairServer)
-            return keyValueStore_pb2.FlagResponse(flag=response.pairsCount)
+        if self._isSecondPart:
+            with grpc.insecure_channel(serviceActivation.serverAddr) as newChannel:
+                stub = keyValueStore_pb2_grpc.CentralServerStub(newChannel)
+                pairServer = keyValueStore_pb2.PairServer()
+                pairServer.serverAddr = self._myAddr
+                keysMessageList = []
+                for key in list(self.pairs.keys()):
+                    pairServer.keys.add().key=key
+                response = stub.Register(pairServer)
+                return keyValueStore_pb2.FlagResponse(flag=response.pairsCount)
+        else:
+            return keyValueStore_pb2.FlagResponse(flag=0)
     
     def Stop(self, stopParams, context):
         self._stop_event.set()
         return keyValueStore_pb2.FlagResponse(flag=0)
 
-def server(serverPort):
+def server(serverPort, isSecondPart):
     stop_event = threading.Event()
     myAddr = f"{socket.getfqdn()}:{str(serverPort)}"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     keyValueStore_pb2_grpc.add_KeyValueStoreServicer_to_server(
-        KeyValueStoreServicer(stop_event, myAddr), server)
+        KeyValueStoreServicer(stop_event, myAddr, isSecondPart), server)
     server.add_insecure_port(f'[::]:{serverPort}')
     server.start()
     stop_event.wait()
     server.stop(grace=1)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
         exit()
 
+    isSecondPart = False
+    if len(sys.argv) == 3:
+        isSecondPart = True
     serverPort = sys.argv[1]
 
-    server(serverPort)
+    server(serverPort, isSecondPart)
